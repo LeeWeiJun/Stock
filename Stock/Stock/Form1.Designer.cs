0f7780d0da709cd86b465a2604af7338dce8b32a
LeeWeiJun <37039828+LeeWeiJun@users.noreply.github.com>
2021-10-21 16:36:02 +0800

    Add files via upload

---
+++ Stock/Stock/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Stock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menu = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Page = new System.Windows.Forms.TextBox();
+            this.Title1 = new System.Windows.Forms.Label();
+            this.Title2 = new System.Windows.Forms.Label();
+            this.Title3 = new System.Windows.Forms.Label();
+            this.Title4 = new System.Windows.Forms.Label();
+            this.Title5 = new System.Windows.Forms.Label();
+            this.Title6 = new System.Windows.Forms.Label();
+            this.Title7 = new System.Windows.Forms.Label();
+            this.Title8 = new System.Windows.Forms.Label();
+            this.Title9 = new System.Windows.Forms.Label();
+            this.Title10 = new System.Windows.Forms.Label();
+            this.Title11 = new System.Windows.Forms.Label();
+            this.Title12 = new System.Windows.Forms.Label();
+            this.Title13 = new System.Windows.Forms.Label();
+            this.next = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.MSG = new System.Windows.Forms.Label();
+            this.check = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.Location = new System.Drawing.Point(12, 103);
+            this.Menu.Multiline = true;
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(1267, 335);
+            this.Menu.TabIndex = 15;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(13, 13);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 2;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Page
+            // 
+            this.Page.Location = new System.Drawing.Point(94, 12);
+            this.Page.Name = "Page";
+            this.Page.Size = new System.Drawing.Size(21, 22);
+            this.Page.TabIndex = 1;
+            this.Page.Text = "1";
+            // 
+            // Title1
+            // 
+            this.Title1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Title1.Location = new System.Drawing.Point(12, 83);
+            this.Title1.Name = "Title1";
+            this.Title1.Size = new System.Drawing.Size(61, 17);
+            this.Title1.TabIndex = 0;
+            this.Title1.Text = "Title1";
+            // 
+            // Title2
+            // 
+            this.Title2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title2.Location = new System.Drawing.Point(113, 84);
+            this.Title2.Name = "Title2";
+            this.Title2.Size = new System.Drawing.Size(61, 17);
+            this.Title2.TabIndex = 0;
+            this.Title2.Text = "Title2";
+            // 
+            // Title3
+            // 
+            this.Title3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title3.Location = new System.Drawing.Point(210, 84);
+            this.Title3.Name = "Title3";
+            this.Title3.Size = new System.Drawing.Size(61, 17);
+            this.Title3.TabIndex = 4;
+            this.Title3.Text = "Title3";
+            // 
+            // Title4
+            // 
+            this.Title4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title4.Location = new System.Drawing.Point(307, 84);
+            this.Title4.Name = "Title4";
+            this.Title4.Size = new System.Drawing.Size(61, 17);
+            this.Title4.TabIndex = 5;
+            this.Title4.Text = "Title4";
+            // 
+            // Title5
+            // 
+            this.Title5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title5.Location = new System.Drawing.Point(401, 84);
+            this.Title5.Name = "Title5";
+            this.Title5.Size = new System.Drawing.Size(61, 17);
+            this.Title5.TabIndex = 6;
+            this.Title5.Text = "Title5";
+            // 
+            // Title6
+            // 
+            this.Title6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title6.Location = new System.Drawing.Point(495, 83);
+            this.Title6.Name = "Title6";
+            this.Title6.Size = new System.Drawing.Size(61, 18);
+            this.Title6.TabIndex = 7;
+            this.Title6.Text = "Title6";
+            // 
+            // Title7
+            // 
+            this.Title7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title7.Location = new System.Drawing.Point(589, 84);
+            this.Title7.Name = "Title7";
+            this.Title7.Size = new System.Drawing.Size(61, 17);
+            this.Title7.TabIndex = 8;
+            this.Title7.Text = "Title7";
+            // 
+            // Title8
+            // 
+            this.Title8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title8.Location = new System.Drawing.Point(687, 84);
+            this.Title8.Name = "Title8";
+            this.Title8.Size = new System.Drawing.Size(61, 17);
+            this.Title8.TabIndex = 9;
+            this.Title8.Text = "Title8";
+            // 
+            // Title9
+            // 
+            this.Title9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title9.Location = new System.Drawing.Point(783, 84);
+            this.Title9.Name = "Title9";
+            this.Title9.Size = new System.Drawing.Size(61, 17);
+            this.Title9.TabIndex = 10;
+            this.Title9.Text = "Title9";
+            // 
+            // Title10
+            // 
+            this.Title10.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title10.Location = new System.Drawing.Point(882, 84);
+            this.Title10.Name = "Title10";
+            this.Title10.Size = new System.Drawing.Size(61, 17);
+            this.Title10.TabIndex = 11;
+            this.Title10.Text = "Title10";
+            // 
+            // Title11
+            // 
+            this.Title11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title11.Location = new System.Drawing.Point(976, 84);
+            this.Title11.Name = "Title11";
+            this.Title11.Size = new System.Drawing.Size(61, 17);
+            this.Title11.TabIndex = 12;
+            this.Title11.Text = "Title11";
+            // 
+            // Title12
+            // 
+            this.Title12.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title12.Location = new System.Drawing.Point(1076, 84);
+            this.Title12.Name = "Title12";
+            this.Title12.Size = new System.Drawing.Size(61, 17);
+            this.Title12.TabIndex = 13;
+            this.Title12.Text = "Title12";
+            // 
+            // Title13
+            // 
+            this.Title13.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Title13.Location = new System.Drawing.Point(1165, 84);
+            this.Title13.Name = "Title13";
+            this.Title13.Size = new System.Drawing.Size(75, 17);
+            this.Title13.TabIndex = 14;
+            this.Title13.Text = "Title13";
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(202, 11);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(75, 23);
+            this.next.TabIndex = 4;
+            this.next.Text = "下一頁";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(121, 13);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 3;
+            this.back.Text = "上一頁";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // MSG
+            // 
+            this.MSG.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.MSG.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MSG.Location = new System.Drawing.Point(385, 152);
+            this.MSG.Name = "MSG";
+            this.MSG.Size = new System.Drawing.Size(275, 90);
+            this.MSG.TabIndex = 18;
+            this.MSG.Text = "MSG";
+            this.MSG.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MSG.Visible = false;
+            // 
+            // check
+            // 
+            this.check.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.check.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.check.Location = new System.Drawing.Point(481, 210);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(75, 23);
+            this.check.TabIndex = 17;
+            this.check.Text = "check";
+            this.check.UseVisualStyleBackColor = false;
+            this.check.Visible = false;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1291, 450);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.Title13);
+            this.Controls.Add(this.Title12);
+            this.Controls.Add(this.Title11);
+            this.Controls.Add(this.Title10);
+            this.Controls.Add(this.Title9);
+            this.Controls.Add(this.Title8);
+            this.Controls.Add(this.Title7);
+            this.Controls.Add(this.Title6);
+            this.Controls.Add(this.Title5);
+            this.Controls.Add(this.Title4);
+            this.Controls.Add(this.Title3);
+            this.Controls.Add(this.Title2);
+            this.Controls.Add(this.Title1);
+            this.Controls.Add(this.Page);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.MSG);
+            this.Controls.Add(this.Menu);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Menu;
+        private System.Windows.Forms.Button Search;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox Page;
+        private System.Windows.Forms.Label Title1;
+        private System.Windows.Forms.Label Title2;
+        private System.Windows.Forms.Label Title3;
+        private System.Windows.Forms.Label Title4;
+        private System.Windows.Forms.Label Title5;
+        private System.Windows.Forms.Label Title6;
+        private System.Windows.Forms.Label Title7;
+        private System.Windows.Forms.Label Title8;
+        private System.Windows.Forms.Label Title9;
+        private System.Windows.Forms.Label Title10;
+        private System.Windows.Forms.Label Title11;
+        private System.Windows.Forms.Label Title12;
+        private System.Windows.Forms.Label Title13;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Label MSG;
+        private System.Windows.Forms.Button check;
+    }
+}
+
---
+++ Stock/Stock/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace Stock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Title1.Text= "代號▼";
+            Title2.Text= "名稱▼";
+            Title3.Text = "價格▼";
+            Title4.Text = "漲跌▼";
+            Title5.Text = "漲跌幅▼";
+            Title6.Text = "周漲跌▼";
+            Title7.Text = "振幅▼";
+            Title8.Text = "開盤▼";
+            Title9.Text = "最高▼";
+            Title10.Text = "最低▼";
+            Title11.Text = "昨收▼";
+            Title12.Text = "成交量▼";
+            Title13.Text = " 成交值(億)▼";
+        }
+
+        string page = "1";
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string page = Page.Text;
+            List<string> list = new List<string>();
+            List<string> list2 = new List<string>();
+            List<string> totli = new List<string>();
+            List<string> totli2 = new List<string>();
+            List<string> sli = new List<string>();
+            HtmlWeb web = new HtmlWeb();
+
+            string[] title = { "代號▼-----\t\r", "名稱▼-----\t\r", "價格▼-----\t\r", "漲跌▼-----\t\r", "漲跌幅▼-----\t\r", "周漲跌▼-----\t\r", "振幅▼-----\t\r", "開盤▼-----\t\r", "最高▼-----\t\r", "最低▼-----\t\r", "昨收▼-----\t\r", "成交量▼-----\t\r", "成交值(億)▼-----" };
+            string[] stringSeparators = new string[] { "\r\n" };
+            string[] stringSeparators2 = new string[] { "</td><td>" };
+            string startline = "</td><td>";
+            string endline = "</td>";
+            string[] remove = new String[] { "最新資訊", "盤後交易量", "台股排行－全部資訊", "全部資訊", "現股當沖", "台股排行榜－價格", "顯示全部", "代號▼名稱▼價格▼漲跌▼漲跌幅▼周漲跌▼振幅▼開盤▼最高▼最低▼昨收▼成交量▼成交值(億)▼" };
+            string up = "上一頁";
+            string down = "下一頁";
+            string[] lines = new string[] { };
+            string[] lines2 = new string[] { };
+
+            Menu.Text = "";
+
+
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://histock.tw/stock/rank.aspx?&p=" + page + "&d=1");
+            HtmlNodeCollection data = doc.DocumentNode.SelectNodes("html/body/form/div[4]/div[5]");
+            foreach (var i in data)
+            {
+                if (i.InnerText.Trim() != "")
+                {
+                    lines = i.InnerText.Split(stringSeparators, StringSplitOptions.None);
+                    lines2 = i.InnerHtml.Split(stringSeparators, StringSplitOptions.None);
+
+                    foreach (var j in lines)
+                    {
+                        if (j.Trim() != "" && remove.Contains(j.Trim()) == false)
+                        {
+                            if (j.Trim().Contains(down) == false && j.Trim().Contains(up) == false)
+                                list.Add(j.Trim());
+                        }
+                    }
+                    foreach (var j in lines2)
+                    {
+                        if (j.Trim().StartsWith(startline) && j.Trim().Length > 10 && j.EndsWith(endline))
+                        {
+                            list2.Add(j.Trim());
+                        }
+                    }
+                }
+            }
+            int con = 0;
+            foreach (var i in list)
+            {
+                string tmp = i;
+
+                switch (con % 10)
+                {
+                    default:
+                        while (tmp.Length <= 11)
+                        {
+                            tmp += "-";
+                        }
+                        break;
+                    case 6:
+                        if (tmp.IndexOf('%') > -1)
+                        {
+                            lines = new string[] { };
+
+                            lines = tmp.Split('%');
+                            lines[0] += "%";
+                            while (lines[0].Length <= 10)
+                            {
+                                lines[0] += "-";
+                            }
+                            while (lines[1].Length <= 10)
+                            {
+                                lines[1] += "-";
+                            }
+                        }
+                        break;
+                }
+                if (lines.Length > 0 && lines.Length < 10)
+                {
+                    foreach (var ii in lines)
+                    {
+                        totli.Add(ii + "\t\r");
+                    }
+                    lines = new string[] { };
+                }
+                else
+                {
+                    if (tmp.Substring(0, 3) == "---" && tmp.EndsWith("-") && tmp.TrimEnd('-').Length>=2)
+                    {
+                        totli.Add(tmp + "\t\t\r");
+                    }
+                    else if(tmp.TrimEnd('-').Length>6 && tmp.Contains("▼") != true && tmp.Contains("▲") != true && tmp.Contains("%") !=true && tmp.EndsWith("+") !=true)
+                    {
+                        if(tmp.TrimEnd('-').Length >= 8)
+                            totli.Add(tmp.TrimEnd('-') + "     \r");
+                        else
+                        totli.Add(tmp.TrimEnd('-') + "\t\r");
+
+                    }
+                    else
+                    totli.Add(tmp + "\t\r");
+                }
+                con++;
+            }
+            lines2 = new string[] { };
+            foreach (var i in list2)
+            {
+                lines2 = i.Split(stringSeparators2, StringSplitOptions.None);
+                lines2[3] = lines2[3].Replace("</td>", "");
+                con = 0;
+                foreach (var j in lines2)
+                {
+                    string tmp = j;
+                    while (tmp.Length <= 11)
+                    {
+                        tmp += "-";
+                    }
+                    lines2[con] = tmp + "\t\r";
+
+                    con++;
+                }
+                totli2.AddRange(lines2);
+                lines2 = new string[] { };
+            }
+            con = 0;
+            int num = 1;
+            foreach (var i in totli2)
+            {
+                if (con < 1)
+                    con++;
+                else if (con >= 1 && con <= 3)
+                {
+                    sli.Add(i);
+                    con++;
+                }
+                if (con == 4)
+                {
+                    totli.InsertRange(10 * num + (4 * (num - 1)), sli);
+                    sli.RemoveRange(0, 3);
+                    num++;
+                    con = 0;
+                }
+            }
+
+            con = 1;
+
+            while (totli.Count >= 13 * con - 1)
+            {
+                totli.RemoveAt(13 * con);
+                con++;
+            }
+
+            con = 1;
+            foreach (var i in totli)
+            {
+                if (con % 13 == 0)
+                {
+                    Menu.Text += i + "\n";
+                }
+                else
+                {
+                    Menu.Text += i;
+                }
+                con++;
+            }
+            list.Clear();
+            list2.Clear();
+            totli.Clear();
+            totli2.Clear();
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            Page.Text = (Convert.ToInt32(Page.Text) + 1).ToString();
+            button1_Click(sender,  e);
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            int page = Convert.ToInt32(Page.Text);
+            if (page >= 2)
+            {
+                Page.Text = (Convert.ToInt32(Page.Text) - 1).ToString();
+                button1_Click(sender, e);
+            }
+            else
+            {
+                MSG.Text = "Page can't < 1";
+                check.Visible = true;
+                MSG.Visible = true;
+            }
+                
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            MSG.Visible = false;
+            check.Visible = false;
+        }
+    }
+}
